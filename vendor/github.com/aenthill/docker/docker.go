3c5c1f1f4d3cf7d1c31f28f6995f225d721b3b03
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-05-22 19:34:29 +0200

    updating docker library

--- vendor/github.com/aenthill/docker/docker.go
+++ vendor/github.com/aenthill/docker/docker.go
@@ -16,9 +16,9 @@ const (
 	// HostProjectDirEnvVariable is the name of the environment variable which contains the host project directory.
 	// You may use this constant with os.Getenv to retrieve the current project directory in your image.
 	HostProjectDirEnvVariable = "AENTHILL_HOST_PROJECT_DIR"
-	// SenderImageEnvVariable is the name of the environment variable which contains the image which sent the event.
+	// SenderEnvVariable is the name of the environment variable which contains the image/binary which sent the event.
 	// You may use this constant with os.Getenv to retrieve the current project directory.
-	SenderImageEnvVariable = "AENTHILL_SENDER_IMAGE"
+	SenderEnvVariable = "AENTHILL_SENDER"
 	// LogLevelEnvVariable sis the name of the environment variable which contains the log level.
 	// You may use this constant with os.Getenv to retrieve the current log level.
 	LogLevelEnvVariable = "AENTHILL_LOG_LEVEL"
@@ -33,7 +33,7 @@ const (
 
 // EventContext gathers all required data of an event.
 type EventContext struct {
-	// WhoAmI is the image which is sending the event.
+	// WhoAmI is the image/binary which is sending the event.
 	WhoAmI string
 	// Image is the image which receives the event.
 	Image string
@@ -51,7 +51,7 @@ Send use the docker client binary to send an event.
 
 It will in fact run a command in the targeted image, using the following template:
 
- docker run [-ti] --rm -v "/var/run/docker.sock:/var/run/docker.sock" -v "hostProjectDir:/aenthill" -e "AENTHILL_SENDER_IMAGE=WhoAmI" -e "AENTHILL_HOST_PROJECT_DIR=HostProjectDir"  -e "AENTHILL_LOG_LEVEL=LogLevel" Image Binary even payload
+ docker run [-ti] --rm -v "/var/run/docker.sock:/var/run/docker.sock" -v "HostProjectDir:/aenthill" -e "AENTHILL_SENDER=WhoAmI" -e "AENTHILL_HOST_PROJECT_DIR=HostProjectDir"  -e "AENTHILL_LOG_LEVEL=LogLevel" Image Binary even payload
 
 Important: it relies on COMSPEC environment variable on Windows and SHELL
 on posix system to know which interpreter to use for calling the docker client
@@ -72,7 +72,7 @@ func Send(event string, payload string, context *EventContext) error {
 	dockerOpts = append(dockerOpts, "--rm")
 	dockerOpts = append(dockerOpts, fmt.Sprintf("-v \"%s:%s\"", dockerSocket, dockerSocket))
 	dockerOpts = append(dockerOpts, fmt.Sprintf("-v \"%s:%s\"", context.HostProjectDir, InsideContainerProjectDir))
-	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", SenderImageEnvVariable, context.WhoAmI))
+	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", SenderEnvVariable, context.WhoAmI))
 	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", HostProjectDirEnvVariable, context.HostProjectDir))
 	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", LogLevelEnvVariable, context.LogLevel))
 
