15321c6c84e010cce3f3eba2382dee2bbfb44e5e
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-05-25 17:27:51 +0200

    updating packages

--- vendor/github.com/aenthill/docker/docker.go
+++ vendor/github.com/aenthill/docker/docker.go
@@ -14,14 +14,18 @@ import (
 
 const (
 	// HostProjectDirEnvVariable is the name of the environment variable which contains the host project directory.
+	// The recipient image will have this variable populated with the HostProjectDir attribute of the EventContext.
 	HostProjectDirEnvVariable = "AENTHILL_HOST_PROJECT_DIR"
 	// SenderEnvVariable is the name of the environment variable which contains the image name of the sender.
+	// The recipient image will have this variable populated with the WhoAmI attribute of the EventContext.
 	SenderEnvVariable = "AENTHILL_SENDER"
 	// LogLevelEnvVariable is the name of the environment variable which contains the log level.
+	// The recipient image will have this variable populated with the LogLevel attribute of the EventContext.
 	LogLevelEnvVariable = "AENTHILL_LOG_LEVEL"
 	// WhoAmIEnvVariable is the name of the environment variable which contains the image name.
-	WhoAmIEnvVariable = "AENTHILL_WHO_AM_I"
-	// InsideContainerProjectDir is the location the the container where the host project directory is mounted.
+	// The recipient and sender images should both have this environment variable populated with their respective image name.
+	WhoAmIEnvVariable = "AENTHILL_WHOAMI"
+	// InsideContainerProjectDir is the location in the container where the host project directory is mounted.
 	InsideContainerProjectDir = "/aenthill"
 	// DefaultBinary is the default binary to call in an image.
 	DefaultBinary = "aent"
@@ -29,7 +33,7 @@ const (
 
 // EventContext gathers all required data of an event.
 type EventContext struct {
-	// WhoAmI is the image/binary which is sending the event.
+	// WhoAmI is the image which is sending the event.
 	WhoAmI string
 	// Image is the image which receives the event.
 	Image string
@@ -38,7 +42,7 @@ type EventContext struct {
 	// HostProjectDir is the project directory on the host.
 	HostProjectDir string
 	// LogLevel is the log level which should be used by the targeted image.
-	// Accepted values for log level: DEBUG, INFO, WARN, ERROR, FATAL, PANIC.
+	// Accepted values for log level: DEBUG, INFO, WARN, ERROR, FATAL.
 	LogLevel string
 }
 
@@ -47,7 +51,13 @@ Send use the docker client binary to send an event.
 
 It will in fact run a command in the targeted image, using the following template:
 
- docker run [-ti] --rm -v "/var/run/docker.sock:/var/run/docker.sock" -v "HostProjectDir:/aenthill" -e "AENTHILL_SENDER=WhoAmI" -e "AENTHILL_HOST_PROJECT_DIR=HostProjectDir"  -e "AENTHILL_LOG_LEVEL=LogLevel" Image Binary even payload
+ docker run [-ti] --rm
+ -v "/var/run/docker.sock:/var/run/docker.sock"
+ -v "HostProjectDir:/aenthill"
+ -e "AENTHILL_SENDER=WhoAmI"
+ -e "AENTHILL_HOST_PROJECT_DIR=HostProjectDir"
+ -e "AENTHILL_LOG_LEVEL=LogLevel"
+ Image Binary even payload
 
 Important: it relies on COMSPEC environment variable on Windows and SHELL
 on posix system to know which interpreter to use for calling the docker client
@@ -61,23 +71,9 @@ func Send(event string, payload string, context *EventContext) error {
 		"paypload": payload,
 	}).Info("sending event")
 
-	var dockerOpts []string
-
-	// attaches Stdin if TTY is a terminal.
-	if isatty.IsTerminal(os.Stdin.Fd()) {
-		dockerOpts = append(dockerOpts, "-ti")
-	}
-
-	dockerOpts = append(dockerOpts, "--rm")
-	dockerOpts = append(dockerOpts, fmt.Sprintf("-v \"%s:%s\"", "/var/run/docker.sock", "/var/run/docker.sock"))
-	dockerOpts = append(dockerOpts, fmt.Sprintf("-v \"%s:%s\"", context.HostProjectDir, InsideContainerProjectDir))
-	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", SenderEnvVariable, context.WhoAmI))
-	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", HostProjectDirEnvVariable, context.HostProjectDir))
-	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", LogLevelEnvVariable, context.LogLevel))
-
 	var args []string
 	args = append(args, []string{"docker", "run"}...)
-	args = append(args, dockerOpts...)
+	args = append(args, buildDockerOpts(context)...)
 	args = append(args, []string{context.Image, context.Binary, event, payload}...)
 
 	var e *exec.Cmd
@@ -96,4 +92,22 @@ func Send(event string, payload string, context *EventContext) error {
 	defer fmt.Println()
 
 	return e.Run()
+}
+
+func buildDockerOpts(context *EventContext) []string {
+	var dockerOpts []string
+
+	// attaches Stdin if TTY.
+	if isatty.IsTerminal(os.Stdin.Fd()) {
+		dockerOpts = append(dockerOpts, "-ti")
+	}
+
+	dockerOpts = append(dockerOpts, "--rm")
+	dockerOpts = append(dockerOpts, fmt.Sprintf("-v \"%s:%s\"", "/var/run/docker.sock", "/var/run/docker.sock"))
+	dockerOpts = append(dockerOpts, fmt.Sprintf("-v \"%s:%s\"", context.HostProjectDir, InsideContainerProjectDir))
+	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", SenderEnvVariable, context.WhoAmI))
+	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", HostProjectDirEnvVariable, context.HostProjectDir))
+	dockerOpts = append(dockerOpts, fmt.Sprintf("-e \"%s=%s\"", LogLevelEnvVariable, context.LogLevel))
+
+	return dockerOpts
 }
