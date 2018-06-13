3430cb2de2fc459096eef2c5956f36f7dbe89886
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-06-13 10:35:22 +0200

    updating godoc

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 /*
-Package main is the entry point of the application.
+Aenthill is a solution for allowing Docker containers to communicate with each other 
+in order to fulfill a task in the current host directory.
 
-TODO
+Each of those Docker containers (called aent) is not aware of its peers. 
+It is however able to recieve and communicate events to them. 
+In other words, an aent is not necessarily smart but 
+it shares a part of the task to handle like an ant within its colony.
+
+Aenthill documentation is hosted at https://aenthill.github.io/.
 */
 package main
 
