53fb1171c99f672deac37d6369aaeaddb11df104
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-07-17 15:50:44 +0200

    adding tests for register job + improving tests for install job

--- jobs/install_test.go
+++ jobs/install_test.go
@@ -18,7 +18,7 @@ func TestNewInstallJob(t *testing.T) {
 func TestInstallJobExecute(t *testing.T) {
 	t.Run("calling Execute from install job with an invalid metadata", func(t *testing.T) {
 		m := manifest.New(manifest.DefaultManifestFileName, afero.NewMemMapFs())
-		j := NewInstallJob([]string{"foo:bar"}, nil, tests.MakeTestContext(t), m)
+		j := NewInstallJob([]string{"FOO:bar"}, nil, tests.MakeTestContext(t), m)
 		if err := j.Execute(); err == nil {
 			t.Error("Execute should have thrown an error as given metadata is not valid")
 		}
@@ -30,9 +30,9 @@ func TestInstallJobExecute(t *testing.T) {
 			t.Error("Execute should have thrown an error as given event is not valid")
 		}
 	})
-	t.Run("calling Execute from install job with an valid parameters", func(t *testing.T) {
+	t.Run("calling Execute from install job with valid parameters", func(t *testing.T) {
 		m := manifest.New(manifest.DefaultManifestFileName, afero.NewMemMapFs())
-		j := NewInstallJob(nil, nil, tests.MakeTestContext(t), m)
+		j := NewInstallJob([]string{"FOO=bar"}, []string{"FOO"}, tests.MakeTestContext(t), m)
 		if err := j.Execute(); err != nil {
 			t.Errorf(`Execute should have thrown an error: got "%s"`, err.Error())
 		}
---
+++ jobs/register_test.go
@@ -0,0 +1,56 @@
+package jobs
+
+import (
+	"testing"
+
+	"github.com/aenthill/aenthill/manifest"
+	"github.com/aenthill/aenthill/tests"
+
+	"github.com/spf13/afero"
+)
+
+func TestNewRegisterJob(t *testing.T) {
+	if j := NewRegisterJob("", "", nil, nil, nil, nil); j == nil {
+		t.Error("NewRegisterJob should not have returned an empty job")
+	}
+}
+
+func TestRegisterJobExecute(t *testing.T) {
+	t.Run("calling Execute from register job with no key in context", func(t *testing.T) {
+		m := manifest.New(manifest.DefaultManifestFileName, afero.NewMemMapFs())
+		j := NewRegisterJob("aent/foo", "FOO", nil, nil, tests.MakeTestContext(t), m)
+		if err := j.Execute(); err == nil {
+			t.Error("Execute should have thrown an error as there is no key in context")
+		}
+	})
+	t.Run("calling Execute from register job with an invalid metadata", func(t *testing.T) {
+		m := manifest.New(manifest.DefaultManifestFileName, afero.NewMemMapFs())
+		key := m.AddAent("aent/foo")
+		ctx := tests.MakeTestContext(t)
+		ctx.Key = key
+		j := NewRegisterJob("aent/bar", "FOO", []string{"FOO:bar"}, nil, ctx, m)
+		if err := j.Execute(); err == nil {
+			t.Error("Execute should have thrown an error as given metadata is not valid")
+		}
+	})
+	t.Run("calling Execute from register job with an invalid event", func(t *testing.T) {
+		m := manifest.New(manifest.DefaultManifestFileName, afero.NewMemMapFs())
+		key := m.AddAent("aent/foo")
+		ctx := tests.MakeTestContext(t)
+		ctx.Key = key
+		j := NewRegisterJob("aent/bar", "FOO", nil, []string{"%FOO%"}, ctx, m)
+		if err := j.Execute(); err == nil {
+			t.Error("Execute should have thrown an error as given event is not valid")
+		}
+	})
+	t.Run("calling Execute from register job with an valid parameters", func(t *testing.T) {
+		m := manifest.New(manifest.DefaultManifestFileName, afero.NewMemMapFs())
+		key := m.AddAent("aent/foo")
+		ctx := tests.MakeTestContext(t)
+		ctx.Key = key
+		j := NewRegisterJob("aent/bar", "FOO", []string{"FOO=bar"}, []string{"FOO"}, ctx, m)
+		if err := j.Execute(); err != nil {
+			t.Errorf(`Execute should have thrown an error: got "%s"`, err.Error())
+		}
+	})
+}
