74575b8dfb12f4918f3f3b6a783ee93349829e30
Julien Neuhart <j.neuhart@thecodingmachine.com>
2018-07-23 15:51:57 +0200

    fixing typo in usage of register command

--- app/commands/register.go
+++ app/commands/register.go
@@ -13,7 +13,7 @@ import (
 func NewRegisterCommand(context *context.Context, m *manifest.Manifest) cli.Command {
 	cmd := cli.Command{
 		Name:      "register",
-		Usage:     "Add a dependency to current aent in the manifest",
+		Usage:     "Adds a dependency to current aent in the manifest",
 		UsageText: "aenthill [global options] register image key [command options]",
 		Action: func(ctx *cli.Context) error {
 			job, err := jobs.NewRegisterJob(ctx.Args().Get(0), ctx.Args().Get(1), ctx.StringSlice("metadata"), context, m)
